c7e8845b23cee77e7a475e6ac765951e087ad8ee
Eslavon <46901350+eslavon@users.noreply.github.com>
2020-01-02 15:03:49 +0300

    UPDATE

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__."/vendor/autoload.php";
 
+use Dotenv\Dotenv;
 use DialogFlow\Client;
 use DialogFlow\Model\Query;
 use DialogFlow\Method\QueryApi;
+
+$dotenv  =  Dotenv::createImmutable ( __DIR__ );
+$dotenv -> load ();
 
 $json = file_get_contents("php://input");
 $data = json_decode($json);
@@ -13,22 +17,22 @@ switch ($data->type) {
         echo "ecab86fa";
         break;
     case "message_new";
-    $user_id = $data->object->message->from_id;
-    $peer_id = $data->object->message->peer_id;
-    $message = $data->object->message->text;
-    $client = new Client('bb07361461f4461f8237521cd2d6706e');
-    $queryApi = new QueryApi($client);
-    $meaning = $queryApi->extractMeaning($message, [
-        'sessionId' =>  '251510315',
-        'lang' => 'ru',
-    ]);
-    $response = new Query($meaning);
-    $result =  $response->getResult();
-    $s = $result->getFulfillment();
-    $params["message"] =  $s->getSpeech();
+        $user_id = $data->object->message->from_id;
+        $peer_id = $data->object->message->peer_id;
+        $message = $data->object->message->text;
+        $client = new Client($_ENV['DIALOG_FLOW_API_KEY']);
+        $queryApi = new QueryApi($client);
+        $meaning = $queryApi->extractMeaning($message, [
+            'sessionId' =>  '251510315',
+            'lang' => 'ru',
+        ]);
+        $response = new Query($meaning);
+        $result =  $response->getResult();
+        $s = $result->getFulfillment();
+        $params["message"] =  $s->getSpeech();
     echo "ok";
-    $params["access_token"] = "93c0f24bbab1e58a14a6cbec5b0fb3e55401b2ce49d4df7990e21e94475e046b5f4a87c425812bd652bf0";
-    $params["v"] = "5.103";
+    $params["access_token"] = $_ENV['VK_ACCESS_TOKEN'];
+    $params["v"] = $_ENV['VK_API_VERSION'];
     $params["peer_id"] = $peer_id;
     $params["random_id"] = mt_rand(0,9999999);
     
