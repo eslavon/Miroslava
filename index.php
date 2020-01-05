ed038222c5809cfbad53d7bbaa3f921d42dd42c4
Eslavon <46901350+eslavon@users.noreply.github.com>
2020-01-05 20:48:53 +0300

    add Chatbase

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ use Dotenv\Dotenv;
 use DialogFlow\Client;
 use DialogFlow\Model\Query;
 use DialogFlow\Method\QueryApi;
+use Eslavon\Miroslava\Chatbase\Chatbase;
 use Eslavon\Miroslava\Vk\VkResponse;
 use Eslavon\Miroslava\Vk\VkRequest;
 
@@ -17,6 +18,7 @@ if ($json == null) {
 }
 $vk_response = new VkResponse($json);
 $vk_request = new VkRequest($_ENV["VK_ACCESS_TOKEN"],$_ENV["VK_API_VERSION"]);
+$chatbase = new Chatbase($_ENV["CHATBASE_TOKEN"]);
 
 
 switch ($vk_response->type) {
@@ -27,6 +29,7 @@ switch ($vk_response->type) {
         echo "ok";
         $client = new Client($_ENV["DIALOG_FLOW_API_KEY"]);
         $queryApi = new QueryApi($client);
+        $chatbase->userMessage($vk_response->user_id,"vk",$vk_response->message,"Location_chat",false,false);
         $meaning = $queryApi->extractMeaning($vk_response->message, [
             'sessionId' =>  '251510315',
             'lang' => 'ru',
@@ -35,6 +38,11 @@ switch ($vk_response->type) {
         $dialogflow_result =  $dialogflow_response->getResult();
         $dialogflow_data = $dialogflow_result->getFulfillment();
         $dialogflow_answer =  $dialogflow_data->getSpeech();
-        $vk_request->sendMessage($vk_response->peer_id,$dialogflow_answer);
+        if ($dialogflow_answer !== "не распознано") {
+            $vk_request->sendMessage($vk_response->peer_id,$dialogflow_answer);
+            $chatbase->agentMessage($vk_response->user_id,"vk",$dialogflow_answer,"Location_chat");
+        } else {
+            $chatbase->agentMessage($vk_response->user_id,"vk",$dialogflow_answer,"Location_chat",true);
+        }
     break;
 }
